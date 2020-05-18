c258ca85434d4b08dc581b8cd39d67b400a7e6f0
Zoia Krasheninnikova <krash76@mail.ru>
2020-05-18 22:32:32 +0200

    task.js changed

---
+++ 3.1-exceptions-closure/task.js
@@ -0,0 +1,64 @@
+
+//---Задача_1---
+
+function parseCount(count) {
+  if (Number.parseInt(count) === NaN) {
+    const parseError = new Error('Невалидное значение');
+    throw parseError;
+  } 
+  return Number.parseInt(count);
+} 
+
+function validateCount(count) {
+  try {
+    let result = parseCount(count);
+    typeof result === 'number';
+    return result; 
+  } 
+  catch (parseError) {
+    return parseError;
+   }
+}
+ 
+//---Задача_2--- 
+
+class Triangle {
+  constructor(a, b, c) {
+    this.a = a,
+    this.b = b,
+    this.c = c 
+  
+  if ((this.a + this.b) < this.c || 
+    (this.b + this.c) < this.a ||
+    (this.a + this.c) < this.b ) {
+      const formError = new Error('Треугольник с такими сторонами не существует');
+      throw formError;
+    } 
+  }
+
+  getPerimeter() {
+    return this.a + this.b + this.c;
+  }
+
+  getArea() {
+    const p = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+    return area.toFixed(3);
+  }
+}
+
+function getTriangle (a, b, c) {
+    try {
+      const triangle = new Triangle ();
+      triangle.getArea();
+      triangle.getPerimeter();
+    }
+    catch(formError) {
+      return (
+        triangle.getArea() = 'Ошибка! Неправильный треугольник',
+        triangle.getPerimeter() = 'Ошибка! Неправильный треугольник'
+      )
+    }
+}
+
+ 
